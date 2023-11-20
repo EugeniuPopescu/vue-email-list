56bf2ce734c17840a3e1c7e37d883b4e69d5618f
Eugen <epopescuu@yahoo.it>
2023-11-20 20:32:02 +0100

    feat: finish api

--- main.js
+++ main.js
@@ -20,21 +20,6 @@ const opzioni = {
     
             }
         }
-    },
-    mounted() {
-        // axios recupera GET request THEN esgui funzione
-        // chiama l'indirzzo, in base alla risposta esgui la funzione e fa cose
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(response => {
-            // response.data = dati ottenuti in risposta dal server
-            // response.status = codice HTTP di risposta (1xx-2xx-3xx-4xx-5xx)
-            // response.statusText = codice HTTP di risposta (es. OK)
-            console.log(response.data);
-
-            // se la risposta è ok
-            if (response.data.success) {
-                this.userName = response.data.response;
-            }
-        });
     }
 };
 
